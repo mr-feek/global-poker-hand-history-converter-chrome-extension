1367c9ac6d8e71a02b82dd63a1f96ddd0086a9e6
Feek <fpm5022@gmail.com>
2018-01-15 23:46:22 -0800

    start work on showing status output

--- src/js/gp_override.js
+++ src/js/gp_override.js
@@ -8,10 +8,12 @@ import bugsnagClient from './bugsnagClient';
     <label>Hands to download, starting with the most recent first (multiples of 50)</label>\
     <input type="number" class="form-control js-hc-hands" value="100" />\
     <a class="button js-hc-convert">DOWNLOAD IN POKER STARS FORMAT</a>\
+    <p>Status: <span class="js-hc-status"></span></p>\
     <div class="js-hc-spinning" style="display:none"><img src="https://cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif" /></div>\
 </div>';
 
     let $historyDateContainer;
+    let $statusContainer;
 
     setInterval(() => {
         if ($('.converter-plugin-area').length > 0) {
@@ -29,6 +31,7 @@ import bugsnagClient from './bugsnagClient';
         $historyDateContainer.append(template);
 
         $('.js-hc-convert').click(onClickDownload);
+        $statusContainer = $('.js-hc-status');
     }
 
     function onClickDownload() {
@@ -38,10 +41,24 @@ import bugsnagClient from './bugsnagClient';
         window.chrome.runtime.sendMessage({action: 'hc.convertHands', options: {handsToFetch}}, response => {
             $('.js-hc-spinning').hide();
 
-            if (!response.success) {
+            if (response && response.success) {
+                $statusContainer.html('Finished');
+            } else {
+                $statusContainer.html('Failure');
                 alert('Something went wrong. Please open an issue at https://github.com/mr-feek/global-poker-hand-history-converter/issues.');
             }
         });
     }
+
+    window.chrome.runtime.onConnect.addListener((port) => {
+        console.log('received connection');
+
+        port.onMessage.addListener((data) => {
+            console.log('received message ' + data.message);
+            if (data.action === 'hc.updateStatus') {
+                $statusContainer.html(data.message);
+            }
+        });
+    });
 })();
 
